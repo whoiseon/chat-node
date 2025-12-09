80ea6fd43ac38446e05dacf190f72c08f024daf6
Inseon Hwang <public.whoiseon@gmail.com>
2025-12-09 16:00:12 +0900

    feat: initialized cache class for using redis

--- packages/server/src/routes/api/v1/user/index.ts
+++ packages/server/src/routes/api/v1/user/index.ts
@@ -6,7 +6,7 @@ import { BusinessError } from '@/lib/middlewares/error';
 
 import { UserService } from '@/services/user.service';
 
-import { UserResponse, UserRole } from '@/types';
+import { UserResponse } from '@/types';
 
 const user = new Router();
 const userService = new UserService();
@@ -15,33 +15,24 @@ const userService = new UserService();
  * 내 정보 조회
  */
 user.get('/me', requireAuth, async (ctx) => {
-  const userId = ctx.state.userId!;
+  const userId = ctx.state.userId;
 
-  const user = await userService.findUser(userId, 'userId', {
-    id: true,
-    username: true,
-    np: true,
-    mainNodeConId: true,
-  });
+  if (!userId) {
+    throw new BusinessError('로그인이 필요합니다.');
+  }
 
-  if (!user) {
+  const me = await userService.findUserWithCache(userId);
+
+  if (!me) {
     throw new BusinessError('존재하지 않는 유저입니다.');
   }
 
-  const adminUser = await userService.findAdminUser(userId);
-
-  let role = UserRole.USER;
-
-  if (adminUser) {
-    role = UserRole.ADMIN;
-  }
-
   ctx.body = generateResponseBody<UserResponse>(true, '', {
-    userId: user.id,
-    username: user.username,
-    np: user.np,
-    mainNodeConId: user.mainNodeConId,
-    role,
+    userId: me.userId,
+    username: me.username,
+    np: me.np,
+    mainNodeConId: me.mainNodeConId,
+    role: me.role,
   });
 });
 
--- packages/server/src/services/user.service.ts
+++ packages/server/src/services/user.service.ts
@@ -1,8 +1,59 @@
 import { Prisma } from 'generated/prisma';
 
 import { prisma } from '@/database';
+import { UserResponse, UserRole } from '@/types';
+import cache from '@/cache';
+import { BusinessError } from '@/lib/middlewares/error';
 
 export class UserService {
+  /**
+   * 유저 정보 조회 (캐시 및 DB 조회)
+   * @param userId
+   */
+  async findUserWithCache(userId: string): Promise<UserResponse | null> {
+    const userKey = cache.generateKey.user(userId);
+    let cachedUser = await cache.get<UserResponse>(userKey);
+
+    if (cachedUser) {
+      // 캐시 히트 시 캐시에서 가져온 정보를 반환
+      return {
+        userId: cachedUser.userId,
+        username: cachedUser.username,
+        np: cachedUser.np,
+        mainNodeConId: cachedUser.mainNodeConId,
+        role: cachedUser.role,
+      };
+    }
+
+    // 캐시 미스 시 DB에서 조회
+    const user = await this.findUser(userId, 'userId', {
+      id: true,
+      username: true,
+      np: true,
+      mainNodeConId: true,
+    });
+
+    if (!user) {
+      throw new BusinessError('존재하지 않는 유저입니다.');
+    }
+
+    const adminUser = await this.findAdminUser(userId);
+    const role = adminUser ? UserRole.ADMIN : UserRole.USER;
+
+    const userResponse: UserResponse = {
+      userId: user.id,
+      username: user.username,
+      np: user.np,
+      mainNodeConId: user.mainNodeConId,
+      role,
+    };
+
+    // 캐시 저장
+    await cache.set<UserResponse>(userKey, userResponse);
+
+    return userResponse;
+  }
+
   async findAdminUser(userId: string) {
     return prisma.adminUser.findUnique({
       where: { userId },
@@ -13,7 +64,7 @@ export class UserService {
   }
 
   /**
-   * 유저 조회
+   * 유저 정보 DB 조회
    */
   async findUser(
     value: string,
