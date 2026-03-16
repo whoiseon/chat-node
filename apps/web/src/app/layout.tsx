fd633881d5735cf405599ed8843cdeddfa9a3dd8
Inseon Hwang <public.whoiseon@gmail.com>
2026-03-16 12:19:20 +0900

    feat: initial commit

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from 'next';
 import '@/styles/globals.css';
+import { Geist } from "next/font/google";
+import { cn } from "@workspace/ui/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -12,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko" suppressHydrationWarning>
+    <html lang="ko" suppressHydrationWarning className={cn("font-sans", geist.variable)}>
       <body>{children}</body>
     </html>
   );
